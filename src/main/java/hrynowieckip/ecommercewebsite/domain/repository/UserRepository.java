cd421fc174b09adf5578b90f17152ea48393633d
Paweł Hrynowiecki <hrynowieckip@gmail.com>
2021-02-08 21:40:04 +0100

    login :: custom user authentication

--- src/main/java/hrynowieckip/ecommercewebsite/domain/repository/UserRepository.java
+++ src/main/java/hrynowieckip/ecommercewebsite/domain/repository/UserRepository.java
@@ -3,7 +3,10 @@ package hrynowieckip.ecommercewebsite.domain.repository;
 import hrynowieckip.ecommercewebsite.domain.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByUsername(String username);
 
+    Optional<User> findByUsername(String username);
 }
