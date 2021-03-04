85c4cf80a50579d725914b340b1de03911023c66
Paweł Hrynowiecki <hrynowieckip@gmail.com>
2021-03-04 19:41:17 +0100

    login-page :: adding own login page

---
+++ src/main/java/hrynowieckip/ecommercewebsite/web/controller/LoginPageController.java
@@ -0,0 +1,20 @@
+package hrynowieckip.ecommercewebsite.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/login")
+public class LoginPageController {
+    @PostMapping
+    public String postLogin() {
+        return "home-page";
+    }
+
+    @GetMapping
+    public String getLogin() {
+        return "login/form";
+    }
+}
