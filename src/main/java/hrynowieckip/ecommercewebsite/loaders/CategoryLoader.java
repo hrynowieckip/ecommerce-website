32300027fcffdb24e2d9f80286f0c31fd4682af0
Paweł Hrynowiecki <hrynowieckip@gmail.com>
2021-04-12 18:15:02 +0200

    basic :: adding category loader, small change in product form html

---
+++ src/main/java/hrynowieckip/ecommercewebsite/loaders/CategoryLoader.java
@@ -0,0 +1,31 @@
+package hrynowieckip.ecommercewebsite.loaders;
+
+import hrynowieckip.ecommercewebsite.domain.model.Category;
+import hrynowieckip.ecommercewebsite.domain.repository.CategoryRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import javax.transaction.Transactional;
+
+@Component
+@Slf4j
+@RequiredArgsConstructor
+public class CategoryLoader implements DataLoader {
+    private final CategoryRepository categoryRepository;
+
+    public int getOrder() {
+        return Integer.MIN_VALUE;
+    }
+
+    @Transactional
+    public void loadData() {
+        Category category = Category.builder()
+                .name("Other")
+                .build();
+
+        categoryRepository.save(category);
+        log.debug("Saved category: {}", category);
+
+    }
+}
